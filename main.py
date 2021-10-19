15f00b982ae45362a06dc8c426b3a8b8f447c1f7
STR-HK <78652371+STR-HK@users.noreply.github.com>
2021-10-19 14:10:49 +0900

    SQLite

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 from fastapi import FastAPI
+import uvicorn
+
+import db
 
 app = FastAPI()
-
-f = open("./pru/publicchat.txt", 'w', encoding='UTF-8')
-f.close()
-
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
+MyDB = db.Database()
+MyDB.connect("./pru/publicchat.sql")
+MyDB.create_table_if_not_exists(table_name='chat', fields=['content'])
 
-@app.get("/publicchat/{content}")
+@app.get("/publicchat/append/{content}")
 async def read_item(content: str):
-    k = open("./pru/publicchat.txt", 'a')
-    print(content)
-    k.write(content + "\n")
-    k.close()
-    k = open("./pru/publicchat.txt", 'r')
-    returnval = k.read()
-    k.close()
-    return {returnval}
+    dic = {}
+    dic['content'] = content
+    MyDB.insert(table_name='chat', values=dic)
+    ret = MyDB.select(table_name='chat', field_names=['content'])
+    rlist = []
+    for r in ret:
+        rlist.append(r[0])
+    return rlist
+
+@app.get('/publicchat/save')
+async def save():
+    MyDB.save()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=7113)
